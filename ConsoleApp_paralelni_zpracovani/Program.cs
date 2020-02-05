dac470453e9d8794c3c53e4688e912b896a7b436
GitHrob <47902917+GitHrob@users.noreply.github.com>
2020-02-05 12:12:21 +0100

    Add files via upload
    init

---
+++ ConsoleApp_paralelni_zpracovani/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_paralelni_zpracovani
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var sw = Stopwatch.StartNew();
+            var tokenSource = new CancellationTokenSource();
+            tokenSource.CancelAfter(2000);
+            var token = tokenSource.Token;
+            token.ThrowIfCancellationRequested();
+
+            Task<int> uloha1async = Task.Run(async () =>
+            {
+                try
+                {
+                    await Task.Delay(6000, token);
+                    Console.WriteLine("uloha 1 splnena - start pred {0} ms", sw.ElapsedMilliseconds);
+                    return 1;
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("uloha 1 chyba zrušena - start pred {0} ms", sw.ElapsedMilliseconds);
+                    return -1;
+                }
+            }, token);
+
+            Task<int> uloha2async = Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine("uloha 2 splnena - start pred {0} ms", sw.ElapsedMilliseconds);
+                return (2);
+            });
+
+            await Task.WhenAll(uloha1async, uloha2async);
+            Task.WaitAll(uloha1async, uloha2async);
+
+            int uloha1Result = await uloha1async;
+            Console.WriteLine("uloha 1 vysledek{1} - start pred {0} ms", sw.ElapsedMilliseconds, uloha1Result);
+            int uloha2Result = await uloha2async;
+            Console.WriteLine("uloha 2 vysledek{1} - start pred {0} ms", sw.ElapsedMilliseconds, uloha2Result);
+
+            Console.WriteLine("Hello World! pred {0} ms", sw.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
